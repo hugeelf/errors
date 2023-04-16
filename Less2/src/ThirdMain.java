e0955f2e6cb7db1b89b2e48ab4a5b45620707e4a
hugeelf <hugeelf@yandex.ru>
2023-04-16 23:16:14 +0300

    add less2 ex4 and some fixes for ex3

--- Less2/src/ThirdMain.java
+++ Less2/src/ThirdMain.java
@@ -10,14 +10,15 @@ public class ThirdMain {
             int[] abc = { 1, 2 };
             abc[3] = 9;
         }
-//        catch (Throwable ex) {
-//            System.out.println("Что-то пошло не так...");
-//        }
+
 //        catch (NullPointerException ex) {
 //            System.out.println("Указатель не может указывать на null!");
 //        }
         catch (IndexOutOfBoundsException ex) {
             System.out.println("Массив выходит за пределы своего размера!");
+        }
+        catch (Throwable ex) {
+            System.out.println("Что-то пошло не так...");
         }
     }
     public static void printSum(Integer a, Integer b) /*throws FileNotFoundException*/ {
